82105acbb182cdc45c3d8850244e752913efa14e
X10-PF4G02JW\X10 <samabat.y@extend-it-resource.com>
2023-10-31 14:47:00 +0700

    code for deploy

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,24 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryEntity } from './entities/category.entity';
 import { ProductEntity } from './entities/product.entity';
 import { ProductImageEntity } from './entities/product_image';
-import { SalesEntity } from './entities/sales.entity';
-import { UserEntity } from './entities/user.entity';
 import { SalesItemEntity } from './entities/sales-item.entity';
+import { SalesEntity } from './entities/sales.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: '45.144.164.18',
       port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'act',
+      username: 'poseggadmin',
+      password: 'YHkxty3UFi48YY4qZ7XjY7Cx9eoiTpTBDSJ7i7RVgvJaVsxrKAp4KTz6',
+      database: 'pos_egg',
       autoLoadEntities: true,
       synchronize: true, //set false for production
     }),
     TypeOrmModule.forFeature([
-      UserEntity,
       CategoryEntity,
       ProductEntity,
       SalesEntity,
--- src/respositories/repositories.module.ts
+++ src/respositories/repositories.module.ts
@@ -1,37 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SalesItemEntity } from 'src/database/entities/sales-item.entity';
 import { DatabaseModule } from '../database/database.module';
 import { CategoryEntity } from '../database/entities/category.entity';
 import { ProductEntity } from '../database/entities/product.entity';
 import { ProductImageEntity } from '../database/entities/product_image';
 import { SalesEntity } from '../database/entities/sales.entity';
-import { UserEntity } from '../database/entities/user.entity';
-import { AuthRepositoryService } from './auth-respository/auth-respository.service';
 import { CategoryRepositoryService } from './category-respository/category-respository.service';
 import { ProductRepositoryService } from './product-respository/product-respository.service';
 import { SaleRepositoryService } from './sale-respository/sale-respository.service';
-import { SalesItemEntity } from 'src/database/entities/sales-item.entity';
 
 @Module({
   imports: [
     DatabaseModule,
     TypeOrmModule.forFeature([
-      UserEntity,
       CategoryEntity,
       ProductEntity,
       ProductImageEntity,
       SalesEntity,
-      SalesItemEntity
+      SalesItemEntity,
     ]),
   ],
   providers: [
-    AuthRepositoryService,
     CategoryRepositoryService,
     ProductRepositoryService,
     SaleRepositoryService,
   ],
   exports: [
-    AuthRepositoryService,
     CategoryRepositoryService,
     ProductRepositoryService,
     SaleRepositoryService,
